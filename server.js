2043dd5c92a1759989d13caf06f297a0c341006f
Scholarbee <99612272+Scholarbee@users.noreply.github.com>
2024-04-10 18:33:02 +0000

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:9002", "http://localhost:5173"],
+    origin: ["http://localhost:9002", "http://localhost:5173","https://sts-inventory.onrender.com"],
     credentials: true,
   })
 );
